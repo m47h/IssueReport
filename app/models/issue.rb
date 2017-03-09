bf12d50617b879af5f3690e9dcce786d36e3ee02
m47h m47h <matekstar@o2.pl>
2017-03-10 00:16:06 +0100

    Create basic paperclip with XHR

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,9 +1,16 @@
 class Issue < ApplicationRecord # :nodoc:
   STATUS = %w(Open Pending Closed).freeze
   PRIORITY = %w(Low Normal High).freeze
-
+  # rubocop:disable AlignHash
   has_many :comments
+  has_attached_file :image,
+                    styles: { thumb: '64x64#' },
+                    default_url: '/system/:class/:attachment/64x64.png'
 
   validates :name, :body, presence: true
   validates :name, uniqueness: true
+  validates_attachment :image, presence: true,
+                       content_type: { content_type: ['image/jpeg', 'image/gif', 'image/png'] },
+                       size: { in: 0..1.megabytes }
 end
+# rubocop:enable AlignHash
