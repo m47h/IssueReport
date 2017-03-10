7de9d69b37897d712b0dc651de45ea68aa9a17e9
m47h m47h <matekstar@o2.pl>
2017-03-10 23:47:53 +0100

    Capybara added to Gemfile && set RSpec as default test

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,16 @@
 require_relative 'boot'
 
 require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module IssueReport
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Europe/Warsaw'
+    config.active_record.default_timezone = :local # Or :utc
+
+    config.generators do |g|
+      g.test_framework :rspec, views: false, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
