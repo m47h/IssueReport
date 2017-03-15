9fa568a5b95abe9ecffaaf3621c2d4e5117c1c31
m47h m47h <matekstar@o2.pl>
2017-03-16 00:16:00 +0100

    Enable RSpec Devise Capybara work with Selenium Javascript Engine

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,37 +5,17 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'devise'
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  # The order of these blocks is very significant. Right now our
-  # js: true block overrides the plain one when a spec is js: true.
-  # If it were reversed, Database Cleaner would always be using transactions
-  # and since our js: true tests can't see those records there would be problems.
-  # Next we have to actually clean up the database:
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-  # This block must be here, do not combine with the other `before(:each)` block.
-  # This makes it so Capybara can see the database.
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
 
 Shoulda::Matchers.configure do |config|
