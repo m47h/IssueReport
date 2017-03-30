6ecbdac4b7bdbbcb63527b8cb11f4733a0f778ef
m47h m47h <matekstar@o2.pl>
2017-03-30 19:00:56 +0200

    Fix to migration Adding User trucking to ISSUE

--- db/migrate/20170329222251_add_user_to_issues.rb
+++ db/migrate/20170329222251_add_user_to_issues.rb
@@ -1,8 +1,8 @@
 class AddUserToIssues < ActiveRecord::Migration[5.0]
   def change
-    add_column :issues, :user_id, :integer, null: false
+    add_column :issues, :user_id, :integer, null: false, default: 1
     Issue.all.each do |issue|
-      issue.update_attributes!(user_id: User.first.id)
+      issue.update_attributes!(user_id: User.first.id || 1)
     end
     add_index :issues, :user_id
   end
--- db/schema.rb
+++ db/schema.rb
@@ -28,13 +28,13 @@ ActiveRecord::Schema.define(version: 20170329222251) do
     t.text     "body"
     t.string   "status"
     t.string   "priority"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.integer  "user_id",            null: false
+    t.integer  "user_id",            default: 1, null: false
     t.index ["user_id"], name: "index_issues_on_user_id", using: :btree
   end
 
