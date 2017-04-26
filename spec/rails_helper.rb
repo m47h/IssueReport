265c8b983d2ad25d29a337e01971b3dbe253304e
m47h m47h <matekstar@o2.pl>
2017-04-26 19:31:57 +0200

    Adjust time to Capybara ajax requests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,3 +35,5 @@ Capybara.register_driver :selenium_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 Capybara.javascript_driver = :selenium_chrome
+# for AjaxRequest
+Capybara.default_max_wait_time = 7
